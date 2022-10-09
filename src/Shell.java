4685cfd494ebe7700c6cf1c6f575a360313e64fe
ryanapplebee12 <ryanapplebee12@gmail.com>
2022-10-08 21:47:33 -0700

    first commit

---
+++ src/Shell.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+class Shell extends Thread {
+    public Shell( ) {
+    }
+    public void run( ) {
+        for ( int line = 1; ; line++ ) {
+            String cmdLine = "";
+            do {
+                StringBuffer inputBuf = new StringBuffer( );
+                SysLib.cerr( "shell[" + line + "]% " );
+                SysLib.cin( inputBuf );
+                cmdLine = inputBuf.toString( );
+            } while ( cmdLine.length( ) == 0 );
+            String[] args = SysLib.stringToArgs( cmdLine );
+            int first = 0;
+            for ( int i = 0; i < args.length; i++ ) {
+                if ( args[i].equals( ";" ) || args[i].equals( "&" )
+                        || i == args.length - 1 ) {
+                    String[] command = generateCmd( args, first, ( i==args.length - 1 ) ? i+1 : i );
+                    if ( command != null ) {
+
+                        // check if command[0] is "exit". If so, get terminated
+                        // otherwise, pass command to SysLib.exec( )
+                        // if args[i]="&" don’t call SysLib.join( ), Otherwise (i.e., ";"), keep calling SysLib.join( )
+
+                    }
+                    first = i + 1; // go on to the next command delimited by ";" or "&"
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        Shell shell = new Shell();
+        shell.run();
+
+    }
+
+    public String[] generateCmd(String[] args, int first, int passed) {
+        return null;
+    }
+}
